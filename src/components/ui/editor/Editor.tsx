3665eeab36950cdf4ecba5d592ca3d1f7c671ca6
ArtemRubtsov <ua.rubtsov.a@gnail.com>
2024-10-03 22:43:50 +0300

    feat marked

--- src/components/ui/editor/Editor.tsx
+++ src/components/ui/editor/Editor.tsx
@@ -1,8 +1,11 @@
+import { marked } from "marked"
 import { Form } from "../../form/Form"
 
 export const Editor = () => {
+    const res = marked.parse('# Marked in browser\n\nRendered by **marked**.');
   return (
     <main>
+        {res}
         <Form />
     </main>
   )
