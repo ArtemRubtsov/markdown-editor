5fe6cf80c6b87eb3bf23c607a814c14b6ad6c2de
ArtemRubtsov <ua.rubtsov.a@gnail.com>
2024-10-04 19:48:59 +0300

    Merge branch

--- src/components/ui/editor/Editor.tsx
+++ src/components/ui/editor/Editor.tsx
@@ -1,8 +1,8 @@
-import { marked } from "marked"
+// import { marked } from "marked"
 import {Form} from '../../form/Form'
 
 export const Editor = () => {
-    const res = marked.parse('# Marked in browser\n\nRendered by **marked**.');
+    // const res = marked.parse('# Marked in browser\n\nRendered by **marked**.');
   return (
     <main>
         {/* {res} */}
