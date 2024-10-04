65aeb63cc36e1778d193fe8f65130b049a807f7b
ArtemRubtsov <ua.rubtsov.a@gnail.com>
2024-10-04 19:44:30 +0300

    change import

--- src/components/ui/editor/Editor.tsx
+++ src/components/ui/editor/Editor.tsx
@@ -1,11 +1,11 @@
 import { marked } from "marked"
-import { Form } from "../../form/Form"
+import {Form} from '../../form/Form'
 
 export const Editor = () => {
     const res = marked.parse('# Marked in browser\n\nRendered by **marked**.');
   return (
     <main>
-        {res}
+        {/* {res} */}
         <Form />
     </main>
   )
