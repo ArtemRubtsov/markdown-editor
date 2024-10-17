3cf57aa6f8f95464b2388dfd70b5e49dc8055262
ArtemRubtsov <ua.rubtsov.a@gmail.com>
2024-10-17 17:40:43 +0300

    feat: added to component react-icons list and style

--- src/components/ui/Header/Header.tsx
+++ src/components/ui/Header/Header.tsx
@@ -1,11 +1,37 @@
+import { BsTypeH1, BsTypeH2, BsTypeH3, BsTypeH4, BsTypeH5, BsTypeH6 } from 'react-icons/bs'
+import { FaBold, FaItalic, FaLink, FaList } from 'react-icons/fa'
+import { GrBlockQuote } from 'react-icons/gr'
+import { ImListNumbered } from 'react-icons/im'
+import { IoCodeSlashSharp } from 'react-icons/io5'
+import { CiImageOn } from 'react-icons/ci'
 import s from './header.module.scss'
+
+const icons = [
+    <BsTypeH1 />,
+    <BsTypeH2 />, 
+    <BsTypeH3 />, 
+    <BsTypeH4 />, 
+    <BsTypeH5 />, 
+    <BsTypeH6 />, 
+    <FaBold />, 
+    <FaItalic />, 
+    <GrBlockQuote />,
+    <ImListNumbered />,
+    <FaList />,
+    <IoCodeSlashSharp />,
+    <CiImageOn />,
+    <FaLink />
+  ]
+
 export const Header = () => {
   return (
-    <ul className={s.ul}>
-        <li>1</li>
-        <li>2</li>
-        <li>3</li>
-        <li>4</li>
+    <ul className={s.ul}>     
+      {
+      icons.map((el, i) => {
+        return  (
+          <li key={i}>{el}</li>
+        )
+      })}
     </ul>
   )
 }
