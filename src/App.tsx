14001a980c155811ea73c5baed282d7c8ed9a6e3
ArtemRubtsov <ua.rubtsov.a@gmail.com>
2024-10-27 17:27:24 +0200

    feat: editorSlice and wrap to App Prrvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
+import { Provider } from "react-redux"
 import { Container } from "./components/Container/Container"
 import { Header } from "./components/Header/Header"
 import { Main } from "./components/Main/Main"
+import { store } from "./app/store"
 
 
 function App() {
   return (
+    <Provider store={store}>
       <Container>
         <Header />
         <Main />
       </Container>
+    </Provider>  
   )
 }
 
